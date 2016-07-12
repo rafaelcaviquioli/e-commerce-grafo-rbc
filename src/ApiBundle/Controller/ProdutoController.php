05174f61ec17028dcbb8804b89d75cb3b10a55b1
rafaelcaviquioli <rafaelcitj@gmail.com>
2016-07-11 21:32:38 -0300

    Trabalhando na api de busca

--- src/ApiBundle/Controller/ProdutoController.php
+++ src/ApiBundle/Controller/ProdutoController.php
@@ -3,8 +3,10 @@
 namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class ProdutoController extends Controller
@@ -32,6 +34,7 @@ class ProdutoController extends Controller
 
     /**
      * @Route("/api/produto/{id}")
+     * @Method("GET")
      * @ApiDoc(
      *  resource=true,
      *  description="Retorna os dados do produto.",
@@ -49,6 +52,47 @@ class ProdutoController extends Controller
             $produto[0]->transformEntities();
 
             return new JsonResponse($produto[0]);
+        }else{
+            return new JsonResponse([]);
         }
+    }
+    /**
+     * @Route("/api/produto_search")
+     * @Method("POST")
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Busca de produtos.",
+     *  filters={
+     *  {"name"="idMarca", "dataType"="integer"},
+     *  {"name"="idTamanho", "dataType"="integer"},
+     *  {"name"="idCategoria", "dataType"="integer"},
+     *  {"name"="idGenero", "dataType"="integer"},
+     *  {"name"="idCor", "dataType"="integer"},
+     *  }
+     * )
+     */
+    public function buscaProdutoAction(Request $request)
+    {
+        $idMarca = $request->request->get('idMarca');
+        $idTamanho = $request->request->get('idTamanho');
+        $idCategoria = $request->request->get('idCategoria');
+        $idGenero = $request->request->get('idGenero');
+        $idCor = $request->request->get('idCor');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $produtos = $em->getRepository('SiteBundle:Produto')->createQueryBuilder('p')
+            ->where("p.idMarca = '$idMarca' OR p.idTamanho = '$idTamanho' OR idCategoria = '$idCategoria' OR idGenero = '$idGenero' OR idCor = '$idCor'")
+            ->getResult();
+
+        if(!count($produtos)){
+            return new JsonResponse([]);
+        }
+
+        foreach($produtos as $produto){
+            $produto->transformEntities();
+        }
+
+        return new JsonResponse($produtos);
     }
 }
